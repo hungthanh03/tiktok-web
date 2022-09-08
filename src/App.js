d3cd2210db555d5661f6fff3a725efb7844ff379
teq-hungthanh <hungthanh@teqnological.asia>
2022-09-08 15:21:29 +0700

    config sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
+
 function App() {
   return (
-    <div className="App">
-      <h1>REACT APP</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            let Layout = DefaultLayout;
+            if (route.Layout) {
+              Layout = route.Layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+            const Page = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,14 @@
+import Sidebar from '~/components/Layout/DefaultLayout/Sidebar';
+import Header from '~/components/Layout/components/Header';
+function DefaultLayout({ children }) {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Sidebar />
+        <div className="content">{children}</div>
+      </div>
+    </div>
+  );
+}
+export default DefaultLayout;
